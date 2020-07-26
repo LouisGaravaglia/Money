47bac327694897dc8825509a7e4ce1c7dbc65065
Louis Garavaglia <louis.garavaglia@gmail.com>
2020-07-26 16:17:13 -0700

    commented out all redundant code

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, render_template, session, make_response, redirect, flash, jsonify
 from random import choice, randint
-from forex_python.converter import CurrencyRates
-from forex_python.converter import CurrencyCodes
+# from forex_python.converter import CurrencyRates
+# from forex_python.converter import CurrencyCodes
 from unittest import TestCase
 from flask_debugtoolbar import DebugToolbarExtension
-from decimal import *
+# from decimal import *
 from currency import CurrencyMethods
 
 app = Flask(__name__)
@@ -16,11 +16,10 @@ app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 debug = DebugToolbarExtension(app)
 
-c = CurrencyRates(force_decimal=True)
-code = CurrencyCodes()
+# c = CurrencyRates(force_decimal=True)
+# code = CurrencyCodes()
 currency_method = CurrencyMethods()
 
-currencies = {"XCD", "MWK", "MGA", "MOP", "MOP", "LRD", "ZAR", "CHF", "KES", "KZT", "JOD", "JPY", "JMD", "ILS", "IQD", "IRR", "IDR", "ISK", "HUF", "HKD", "HNL", "HTG", "GYD", "XOF", "GNF", "GBP", "GTQ", "XCD", "DKK", "GIP", "GHS", "GEL", "GMD", "XAF", "XPF", "FJD", "DKK", "FKP", "ETB", "ERN", "XAF", "SVC", "EGP", "DOP", "XCD", "DJF", "DKK", "XOF", "CZK", "ANG", "CUP", "CUC", "HRK", "CRC", "NZD", "XAF", "CDF", "KMF", "COU", "COP", "AUD", "CNY", "CLP", "CLF", "XAF", "KYD", "CAD", "XAF", "KHR", "CVE", "BIF", "XOF", "BGN", "BND", "BRL", "NOK", "BWP", "BAM", "BOV", "BOB", "INR", "BTN", "BMD", "XOF", "BZD", "BYN", "BBD", "BDT", "BHD", "BSD", "AZN", "AUD", "AWG", "AMD", "ARS", "XCD", "AOA", "EUR", "USD", "DZD", "ALL", "AFN"}
 
 
 @app.route('/')
@@ -46,14 +45,14 @@ def result_page():
     # else:
     #     flash("The converting from currency code is not valid.", "error")
     
-    currency_method.checking_converting_from(currencies, start_curr)
+    currency_method.checking_converting_from(start_curr)
     
     # if end_curr in currencies:
     #     end_symbol = code.get_symbol(end_curr)  
     # else:
     #     flash("The converting to currency code is not valid.", "error")
     
-    end_symbol = currency_method.checking_converting_to(currencies, end_curr)
+    end_symbol = currency_method.checking_converting_to(end_curr)
     
     # if amount == "" or amount == "0":
     #     flash("Not a valid amount", "error")  
@@ -64,7 +63,7 @@ def result_page():
     #     result = c.convert(f'{start_curr}', f'{end_curr}', Decimal(f'{amount}'))
     #     rounded = round(result,2)
     
-    rounded = currency_method.checking_all(start_curr, currencies, end_curr, amount)
+    rounded = currency_method.checking_all(start_curr, end_curr, amount)
    
     
 
--- currency.py
+++ currency.py
@@ -9,13 +9,16 @@ from decimal import *
 code = CurrencyCodes()
 c = CurrencyRates(force_decimal=True)
 
+currencies = {"XCD", "MWK", "MGA", "MOP", "MOP", "LRD", "ZAR", "CHF", "KES", "KZT", "JOD", "JPY", "JMD", "ILS", "IQD", "IRR", "IDR", "ISK", "HUF", "HKD", "HNL", "HTG", "GYD", "XOF", "GNF", "GBP", "GTQ", "XCD", "DKK", "GIP", "GHS", "GEL", "GMD", "XAF", "XPF", "FJD", "DKK", "FKP", "ETB", "ERN", "XAF", "SVC", "EGP", "DOP", "XCD", "DJF", "DKK", "XOF", "CZK", "ANG", "CUP", "CUC", "HRK", "CRC", "NZD", "XAF", "CDF", "KMF", "COU", "COP", "AUD", "CNY", "CLP", "CLF", "XAF", "KYD", "CAD", "XAF", "KHR", "CVE", "BIF", "XOF", "BGN", "BND", "BRL", "NOK", "BWP", "BAM", "BOV", "BOB", "INR", "BTN", "BMD", "XOF", "BZD", "BYN", "BBD", "BDT", "BHD", "BSD", "AZN", "AUD", "AWG", "AMD", "ARS", "XCD", "AOA", "EUR", "USD", "DZD", "ALL", "AFN"}
+
+
 class CurrencyMethods():
 
     # def __init__(self):
 
     #     self.words = self.read_dict("words.txt")
 
-    def checking_converting_from(self, currencies, start_curr):
+    def checking_converting_from(self, start_curr):
         """Read and return all words in dictionary."""
 
         if start_curr in currencies:
@@ -24,7 +27,7 @@ class CurrencyMethods():
             flash("The converting from currency code is not valid.", "error")
             
 
-    def checking_converting_to(self, currencies, end_curr):
+    def checking_converting_to(self, end_curr):
         """Read and return all words in dictionary."""
 
         if end_curr in currencies:
@@ -42,7 +45,7 @@ class CurrencyMethods():
             flash("Not a valid amount", "error")  
             
 
-    def checking_all(self, start_curr, currencies, end_curr, amount):
+    def checking_all(self, start_curr, end_curr, amount):
         """Read and return all words in dictionary."""
 
         rounded = "..."
